b7213deba4d8635c47eb45d8b9dc78b17ec31f4d
Isaque Coelho <isaquecoelho.dj@gmail.com>
2022-07-12 20:22:21 -0300

    useState

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
-import Header from "./componets/Header"
-import SalesCard from "./componets/SalesCard"
+import Header from "./componets/Header";
+import SalesCard from "./componets/SalesCard";
 
 function App() {
   return (
@@ -16,4 +16,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
