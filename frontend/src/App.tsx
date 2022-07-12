10f39e3393dd81cbb2c24d37ec3fa32210193e00
Isaque Coelho <isaquecoelho.dj@gmail.com>
2022-07-12 15:42:42 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,20 @@
+import NotificationButton from "./componets/NotificationButton"
+
+
 function App() {
-  return(
-    <h1>Olá mundo</h1>
+  return (
+    <>
+      <h1>Olá mundo</h1>
+      <NotificationButton />
+      <NotificationButton />
+      <NotificationButton />
+      <NotificationButton />
+      <NotificationButton />
+      <NotificationButton />
+      <NotificationButton />
+      <NotificationButton />
+      <NotificationButton />
+    </>
   )
 }
 
